a5de795cd1ce6448ef0d585e8cde0326b1408f4d
ToniDevStyle <tvr9869@gmail.com>
2024-10-03 09:27:51 +0200

    Finalizacion ejercicio 3

--- SemaphoresExercises/1ProblemSemaphores/src/SistemaGestionFabricaDulces.java
+++ SemaphoresExercises/1ProblemSemaphores/src/SistemaGestionFabricaDulces.java
@@ -3,11 +3,15 @@ import java.util.concurrent.Semaphore; // Importamos Semaphore para la gestión 
 
 public class SistemaGestionFabricaDulces {
 
+    //VARIABLES COMPARTIDAS SON LOS MISMOS SEMAFOROS EN ESTE CASO
+
     // Cinta transportadora con capacidad de 10 dulces (semaforo que limita el acceso a 10).
+    // BandConveyor - 10 sweets limit
     private static Semaphore bandConveyor = new Semaphore(10, true);
     
     // Semáforo para rastrear la cantidad de dulces disponibles en la cinta (inicialmente 0).
     // Funciona a la inversa que el otro emaforo, en vez de ir consumiendo, va generando
+    // Tracking how many sweets are available on the band conveyor
     private static Semaphore availableSweets = new Semaphore(0, true);
     
     // Clase interna para el hilo productor (Tipo 0).
@@ -15,28 +19,33 @@ public class SistemaGestionFabricaDulces {
         private int id; // ID del hilo
 
         // Constructor que asigna el ID al hilo productor.
+        // Constructor ID
         public Type0Thread(int id) {
-            this.id = id; // Asignamos el ID recibido al atributo id.
+            this.id = id; 
         }
 
-        // Método que se ejecuta cuando se inicia el hilo.
+        
         @Override
         public void run() {
             try {
-                
+                // Thread starting, productor is making a sweet
                 System.out.println("El productor " + id + " está produciendo un dulce");
 
                 // El productor intenta adquirir un permiso del semáforo de la cinta transportadora.
+                // Getting the bandConveyor sempahore key if its possible
                 bandConveyor.acquire(); 
                 
                 // Simulamos un tiempo de producción aleatorio (hasta 5000 ms).
+                // Random production time
                 int sleepTime = new Random().nextInt(5000);
                 Thread.sleep(sleepTime);
 
                 // Mensaje indicando que el productor ha producido un dulce.
+                // Productor has endend the sweet
                 System.out.println("El productor " + id + " ha producido un dulce");
                 
                 // El productor libera un dulce, indicando que hay un dulce disponible para empaquetar.
+                // Releasing the sweet, It is available to pack
                 availableSweets.release(); 
             } catch (InterruptedException e) {
                 e.printStackTrace(); 
@@ -53,14 +62,17 @@ public class SistemaGestionFabricaDulces {
             this.id = id; 
         }
 
-        // Método que se ejecuta cuando se inicia el hilo.
+       
         @Override
         public void run() {
             try {
+                // No lo hacemos con tryAcquire porque el enunciado no menciona explicitamente que mientras el consumidor espera, deba hacer algo como por ejemplo decir que esta esperando
+                // Simplemente el consumidor espera a que haya un dulce
                 // El consumidor intenta adquirir un dulce de la cinta transportadora.
+                // Consumer trying to get a sweet from the band conveyor
                 availableSweets.acquire();
                 
-                
+                //Consumer packing the sweet
                 System.out.println("El consumidor " + id + " está empaquetando un dulce");
 
                 
@@ -71,6 +83,7 @@ public class SistemaGestionFabricaDulces {
                 System.out.println("El consumidor " + id + " ha terminado de empaquetar un dulce");
 
                 // El consumidor libera un permiso en la cinta, indicando que se ha retirado un dulce.
+                // Consumer release a key from the bandConveyor
                 bandConveyor.release(); 
             } catch (InterruptedException e) {
                 e.printStackTrace(); 
@@ -99,4 +112,4 @@ public class SistemaGestionFabricaDulces {
 
 
 // Cuando usas acquire(), el hilo que llama a este método se bloquea (se detiene) si no hay permisos disponibles en el semáforo. 
-// El método tryAcquire() no bloquea el hilo. En su lugar, verifica inmediatamente si hay permisos disponibles. Si no hay, devuelve false, lo que significa que el hilo no se detendrá y continuará ejecutándose
+// El método tryAcquire() no bloquea el hilo. En su lugar, verifica inmediatamente si hay permisos disponibles. Si no hay, devuelve false, lo que significa que el hilo no se detendrá y continuará ejecutándose realizando otra accion
